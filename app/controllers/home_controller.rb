f43b4035c70ffebc7a2f0defbb1a106df4fe3ab8
soma335 <56187033+soma335@users.noreply.github.com>
2020-11-16 13:16:00 +0900

    なぜかHomeControllerのテーブルクリエイトの記述が消えていた為修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
     @east = Combat.joins(:user).where(users: {belong:0}).sum(:battle_record)
     @west = Combat.joins(:user).where(users: {belong:1}).sum(:battle_record)
     @battle_data = {'東軍' => @east, '西軍' => @west}
+    if user_signed_in?
+      @user_name = current_user.name
+      unless Combat.find_by_id(user_id:current_user.id)
+        @test = Combat.create(user_id: current_user.id)
+      end
+    end
   end
 
   def show
