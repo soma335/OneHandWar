752925a8fac9f6ad31de59edc1333737178c7d22
soma335 <56187033+soma335@users.noreply.github.com>
2020-11-17 04:25:47 +0900

    本番環境テスト用にbyebug追記

--- app/controllers/api/combats_controller.rb
+++ app/controllers/api/combats_controller.rb
@@ -19,18 +19,19 @@ class Api::CombatsController < ApplicationController
   def create
     combat = Combat.find(current_user.id)
     if combat.update(combat_params)
-    @battle_all = Combat.all.sum(:battle_record)
-    @east_strength = Combat.joins(:user).where(users: {belong:0}).sum(:battle_record)
-    @west_strength = Combat.joins(:user).where(users: {belong:1}).sum(:battle_record)
-    @battle_record = Combat.joins(:user).where(users: {id:current_user.id}).sum(:battle_record)
-    @belong = current_user.belong
-    @user_name = current_user.name
-    respond_to do |f|
-      f.any {
-        render json: [ battle_all: @battle_all.to_json, east_strength: @east_strength.to_json,
-                       west_strength: @west_strength.to_json, battle_record: @battle_record.to_json,
-                       belong: @belong.to_json, user_name: @user_name.to_json ] }
-    end
+      @battle_all = Combat.all.sum(:battle_record)
+      @east_strength = Combat.joins(:user).where(users: {belong:0}).sum(:battle_record)
+      @west_strength = Combat.joins(:user).where(users: {belong:1}).sum(:battle_record)
+      @battle_record = Combat.joins(:user).where(users: {id:current_user.id}).sum(:battle_record)
+      @belong = current_user.belong
+      @user_name = urrent_user.name
+      byebug
+      respond_to do |f|
+        f.any {
+          render json: [ battle_all: @battle_all.to_json, east_strength: @east_strength.to_json,
+                         west_strength: @west_strength.to_json, battle_record: @battle_record.to_json,
+                         belong: @belong.to_json, user_name: @user_name.to_json ] }
+      end
     else
       @battle_all = "Error"
       render json: @battle_all
